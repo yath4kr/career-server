d9a18e53aec198e382eb3f029902635c9abf331a
Yath4kr <yath4kr@gmail.com>
2024-06-23 14:58:01 +0530

    connected to mongoose

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 const port = 5000;
+
+const mongo_URL = process.env.MONGO_URL;
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(mongo_URL)
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
